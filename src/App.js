5c14e7508ad36c8b7e05f01f0ad92cf282c04998
Samilly <samillynunes19@gmail.com>
2024-04-17 15:10:11 -0300

    Main app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import CookieBtn from "./components/CookieBtn";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor (props){
+    super(props);
+    this.state={
+      currentPhrase: '',
+    }
+
+    this.breakCookie = this.breakCookie.bind(this);
+
+    this.phrases= [
+      'Siga os bons e aprenda com eles.', 
+      'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+  }
+
+  breakCookie(){
+    let state = this.state;
+    let randomNumber = Math.floor(Math.random() * this.phrases.length);
+    state.currentPhrase = '" '+ this.phrases[randomNumber] + ' "';
+    this.setState(state);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className="img" />
+        <CookieBtn title="Abrir Biscoito" action={this.breakCookie} />
+        <h3 className="phrase">{this.state.currentPhrase}</h3>
+
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
